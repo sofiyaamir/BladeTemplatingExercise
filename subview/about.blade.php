86bc649362da49cd06279d3e04ad97fbfcc92515
Sofiya <55783291+sofiyaamir@users.noreply.github.com>
2020-12-09 02:34:11 +0800

    Add files via upload

---
+++ subview/about.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>e-Book Sharing Application</title>
+</head>
+<body>
+
+    <h1><center> e-Book Exchange is an online app platform for sharing books.</center></h1>
+
+    <p><center>This app allows you to share books online with readers you don’t necessarily know. <br>
+    <br>Our motive is to boost the habit of reading and spread the 
+    word for all to enter the magical world of books, <br>
+    <br>As books force you to slow down and see 
+    the world through the eyes of your mind. <br>
+    <br><br>So, lose yourselves in books app,
+     because that’s where you will find yourself too!</center></p>
+
+</body>
+</html>
---
+++ subview/list.blade.php
@@ -0,0 +1,54 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>e-Book Sharing Application</title>
+</head>
+<body>
+<h2><u><b>List of e-Book Exchange:</b> </u></h2>
+<ol>
+    <li><b>A Promised Land</b></li>
+    <ul>
+        <li>Author: <b> Barack Obama </b></li>
+        <li>ISBN: <b> 9780241491515 </b></li>
+        <li>Category: <b> Biography & Memoir</b></li>
+        <li>Pages: <b> 768</b></li>
+    </ul>
+    <br>
+    <li><b>Frankenstein</b></li>
+    <ul>
+        <li>Author: <b>Mary Shelley</b></li>
+        <li>ISBN: <b>9780486282114</b></li>
+        <li>Category: <b>Fiction & Literature</b></li>
+        <li>Pages:<b> 280</b></li>
+    </ul>
+    <br>
+    <li><b>The Law of Innocence</b></li>
+    <ul>
+        <li>Author: <b> Michael Connelly</b></li>
+        <li>ISBN: <b> 9780316485623</b></li>
+        <li>Category: <b> Mystery & Suspense</b></li>
+        <li>Pages: <b> 432</b></li>
+    </ul>
+    <br>
+    <li><b>Think and Grow Rich</b></li>
+    <ul>
+        <li>Author: <b>Napoleon Hill</b></li>
+        <li>ISBN: <b>9781906465599</b></li>
+        <li>Category:<b> Business & Finance</b></li>
+        <li>Pages:<b> 238</b></li>
+    </ul>
+    <br>
+    <li> <b>Hark! A Vagrant</b></li>
+    <ul>
+        <li>Author: <b> Kate Beaton</b></li>
+        <li>ISBN:<b> 9781770460607</b></li>
+        <li>Category:<b> Comics & Graphic Novels</b></li>
+        <li>Pages: <b>175 </b></li>
+    </ul>
+    <br>
+
+</ol>
+ 
+</body>
+</html>
